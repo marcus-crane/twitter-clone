ed9b1b5f710f111043cde715d91294c368138e30
Marcus <marcus@thingsima.de>
2016-12-18 17:37:41 +1300

    Posting the form data works plus fixed a little bug in the form

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs');
 app.set('views', (__dirname + '/views'));
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.render('tweets');
 })
 
 app.post('/tweets/create', (req, res) => {
+    let username = req.body.username;
+    let message = req.body.message;
+
+    console.log(`Username: ${username}`)
+    console.log(`Message: ${message}`)
+    
     res.send('Creating tweet...');
 })
 
