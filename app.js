9d1ecfc8dd345579882c4a80e40f19768e33308f
Marcus <marcus@thingsima.de>
2016-12-18 19:09:09 +1300

    Tweets are now in descending order

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    knex('tweets').select()
+    knex('tweets').select().orderBy('id', 'desc')
     .then((query) => {
         for (i in query) {
             query[i].time_from_now = moment(query[i].created_at).fromNow();
